1983497480e1d557b25dec4ff59b7e744f1a9a90
朗 杨 <ULeang@outlook.com>
2022-12-29 20:54:27 +0800

    新增 重载，修改 通用引用 示例

--- UniversalReference/Source.cpp
+++ UniversalReference/Source.cpp
@@ -25,10 +25,15 @@ int main()
 	const int xx = x;
 	auto&& xxx = 17;//universal reference
 	auto&& xxxx = xxx;//universal reference
+	int& xxxxx = x;
 	print(x);
 	print(xx);
 	print(xxx);
-	print(std::forward<int>(xxx));
+	print(std::forward<int>(xxxxx));//左值引用->右值引用
+	print(std::forward<int>(xxx));//右值引用->右值引用
+	print(std::forward<int>(x));//非引用左值->右值引用
+	print(std::forward<int>(1));//非引用右值->右值引用
+	//实际上，forward的结果仅由其模板参数（这里即int）确定（T->T&&,T&->T&），在不需要转发的地方使用forward非常意义不明
 	print(13);
 	return 0;
 }
---
+++ overload/Source.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+class Base
+{
+public:
+	void foo()
+	{
+		std::cout << "Base\n";
+	}
+
+	virtual void foo_2()
+	{
+		std::cout << "Base_2\n";
+	}
+
+	virtual void foo_3()
+	{
+		std::cout << "Base_3\n";
+	}
+};
+
+class Derived :public Base
+{
+public:
+	void foo()
+	{
+		std::cout << "Derived\n";
+	}//覆盖了Base的foo
+
+	virtual void foo_2()
+	{
+		std::cout << "Derived_2\n";
+	}//正确的重载
+
+	virtual void foo_3(int a)
+	{
+		std::cout << "Derived_3\n";
+	}//错误的重载,覆盖了Base的foo_3
+};
+
+int main()
+{
+	Base b;
+	Derived d;
+	b.foo();
+	b.foo_2();
+	b.foo_3();
+	d.foo();
+	d.foo_2();
+	d.foo_3(1);
+
+	Base* pb = &d;
+	pb->foo();//foo不是虚函数，因此调用Base::foo()
+	pb->foo_2();//foo_2是虚函数且被重载了，因此调用Derived::foo_2()
+	pb->foo_3();//foo_3是虚函数但没有被重载，因此表现如同Base::foo_3()
+
+	return 0;
+}
