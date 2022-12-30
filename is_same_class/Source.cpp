dee5a723623d7a758ebbd1fdad2418c3b00bcdbe
ULya午夜曦月 <ULeang@outlook.com>
2022-12-30 19:26:15 +0800

    新增 标准化的示例

--- is_same_class/Source.cpp
+++ is_same_class/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <type_traits>
 
 template<typename T, typename U>
 class is_same_class
@@ -8,14 +9,22 @@ public:
 };
 
 template<typename T>
-class is_same_class<T,T>
+class is_same_class<T, T>
 {
 public:
 	constexpr static bool value = true;
 };
 
+//new style
+template<typename T, typename U>
+struct is_same :std::false_type {};
+
+template<typename T>
+struct is_same<T, T> :std::true_type {};
+
 int main()
 {
 	std::cout << is_same_class<int, int&>::value;
+	std::cout << is_same<int, int&>::value;
 	return 0;
 }
