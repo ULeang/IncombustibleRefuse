ca80950b28605daf379d3aa1ad658748ac02d2d1
朗 杨 <ULeang@outlook.com>
2022-12-30 16:53:26 +0800

    重命名 重载；增加 VauleTraits示例

--- ValueTraits/Source.cpp
+++ ValueTraits/Source.cpp
@@ -1,5 +1,6 @@
 #include <utility>
 #include <vector>
+#include <iterator>
 #include <iostream>
 
 template<typename Iter>
@@ -12,11 +13,7 @@ auto accum_v1(Iter begin, Iter end)
 }//基础版本，不使用traits技术，有bug
 
 template<typename T>
-struct accumulation_traits
-{
-	using type = T;
-	inline static const type zero{};
-};
+struct accumulation_traits;//不提供泛化
 
 template<>
 struct accumulation_traits<int>
@@ -74,10 +71,39 @@ auto accum_v3(Iter begin, Iter end)
 	return res;
 }//v3，使用traits::type和traits::zero
 
+/*************************************************************************/
+//template<typename Iter>
+//struct remove_cvrefiterator
+//{
+//private:
+//	Iter* p = nullptr;
+//public:
+//	using type = std::remove_cvref_t<decltype(**p)>;
+//};//有没有必要delete构造函数呢，待定
+////只接受迭代器作为模板参数！
+//
+//template<typename Iter, typename Traits = accumulation_traits<typename remove_cvrefiterator<Iter>::type>>
+//auto accum_v4(Iter begin, Iter end)
+//{
+//	auto res = Traits::zero;
+//	for (; begin != end; ++begin)
+//		res = res + *begin;
+//	return res;
+//}//v4，使用参数化的萃取
+
+template<typename Iter, typename Traits = accumulation_traits<typename std::iterator_traits<Iter>::value_type>>//Iter非迭代器触发sfinae,然而并没有其他重载
+auto accum_v4(Iter begin, Iter end)
+{
+	auto res = Traits::zero;
+	for (; begin != end; ++begin)
+		res = res + *begin;
+	return res;
+}//v4，使用参数化的萃取
+
 int main()
 {
 	int iarr[]{ 1,2,3,4,5 };
-	std::vector viarr{ 1,2,3,4,5 };
+	std::vector viarr{ 7,2,36,14,49 };
 	char carr[]{ 'a', 'b', 'c', 'd', 'e' };
 	std::cout << accum_v1(iarr, iarr + 5) << std::endl;
 	std::cout << accum_v1(viarr.cbegin(), viarr.cend()) << std::endl;
@@ -88,6 +114,9 @@ int main()
 	std::cout << accum_v3(iarr, iarr + 5) << std::endl;
 	std::cout << accum_v3(viarr.cbegin(), viarr.cend()) << std::endl;
 	std::cout << static_cast<char>(accum_v3(carr, carr + 5) / 5) << std::endl;//正确
+	std::cout << accum_v4(iarr, iarr + 5) << std::endl;
+	std::cout << accum_v4(viarr.cbegin(), viarr.cend()) << std::endl;
+	std::cout << static_cast<char>(accum_v4(carr, carr + 5) / 5) << std::endl;//正确
 
 	return 0;
 }
